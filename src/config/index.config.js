89ae36c2e65d167287c252e04cb7ccdb56822504
AnuragTh09 <thakuranurag432@gmail.com>
2024-03-11 13:01:52 +0530

    forgot password functionality

--- src/config/index.config.js
+++ src/config/index.config.js
@@ -5,6 +5,10 @@ dotenv.config({
 
 const config = {
     PORT: process.env.PORT || 5000,
+    // MONGODB_URL:
+
+    JWT_SECRET: process.env.JWT_SECRET || "teramerajwttoken07",
+    JWT_EXPIRY: process.env.JWT_EXPIRY || "30d"
 }
 
 export default config
